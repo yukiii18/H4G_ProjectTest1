81ee0a1c80b947daa57693b21e0bd9e8ce1d0c0c
yukiii18 <110827731+yukiii18@users.noreply.github.com>
2026-01-20 12:29:32 +0800

    Create NotificationHub.cs

---
+++ H4G_Project/NotificationHub.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace H4G_Project.NotificationHub
+{
+    public class NotificationHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
